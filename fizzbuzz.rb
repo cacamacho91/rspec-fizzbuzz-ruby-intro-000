8152f0556bcaef66de08a46122a5f05d1942cff2
dashing-frame-3501 <cacamacho91@gmail.com>
2018-11-20 09:24:51 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,5 +3,13 @@
 def fizzbuzz(num)
   if num % 3 == 0 && num % 5 == 0
     "FizzBuzz"
+  elsif num % 3 == 0
+    "Fizz"
+  end
+  elsif num % 5 == 0
+    "Buzz"
+  end
+  else
+    return nil
   end
 end
